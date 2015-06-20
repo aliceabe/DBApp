87a03b25d8b57bc85d999c34426f1a09974e7fd5
tupurcoder <chakraborty.esha@gmail.com>
2015-06-20 18:32:53 -0400

    commit-jung 20 6:32

--- routes/byroute.js
+++ routes/byroute.js
@@ -11,6 +11,7 @@ var sql = {
 	'SELECT R.airTime, R.distance \
 	FROM route R, airport A1, airport A2 \
 	WHERE A1.airportId = R.orig AND A2.airportId = R.dest AND A1.airportCode = ? and A2.airportCode = ?; \
+
 	CREATE OR REPLACE VIEW query AS \
 	SELECT F.* \
 	FROM route R, airport A1, airport A2, flight F \
@@ -21,6 +22,7 @@ var sql = {
 		A2.airportCode = ? AND \
 		F.routeId = R.routeId \
 	); \
+
 	SELECT Air.description, A.tot, IFNULL(B.del,0) AS del, IFNULL(C.can,0) AS can, IFNULL(ROUND(B.del*100/A.tot,1),0) AS del_p, IFNULL(ROUND(C.can*100/A.tot,1),0) AS can_p, IFNULL(D.avg,0) AS avg \
 	FROM \
 		(SELECT Q.airlineId, count(*) AS tot FROM query Q GROUP BY Q.airlineId) A LEFT OUTER JOIN \
@@ -29,6 +31,7 @@ var sql = {
 		(SELECT Q.airlineId, ROUND(AVG(FD.duration)) AS avg FROM query Q, flight_delayed FD WHERE Q.flightId=FD.flightId GROUP BY Q.airlineId) D ON A.airlineId = D.airlineId LEFT OUTER JOIN \
 		airline Air ON A.airlineId = Air.airlineId \
 	ORDER BY A.tot DESC; \
+	
 	DROP VIEW IF EXISTS query;'
 
 }
