635d0729cf501c24938336399f9225750d35dd1a
Alice Berard <ab.berard@gmail.com>
2015-06-20 22:47:12 -0400

    changed database

--- routes/connection.js
+++ routes/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host     : 'cs4111.ciu0ibzbcgnr.us-west-2.rds.amazonaws.com',
   user     : 'ec3074',
   password : 'esha1234',
-  database : 'sandbox',
+  database : 'stats',
   multipleStatements: true
 })
 
