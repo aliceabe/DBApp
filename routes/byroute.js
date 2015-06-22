ddf5f76bd404051269b5ee4b2992be3a94bf2fbe
Alice Berard <ab.berard@gmail.com>
2015-06-22 15:30:03 -0400

    Fixed some bugs; added average price

--- routes/byroute.js
+++ routes/byroute.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var mysql = require('mysql')
 var connection = require('./connection')
+var _ = require('underscore')
 var router = express.Router()
 
 var sql = {
@@ -65,6 +66,10 @@ var sql = {
 	) A LEFT JOIN delaycancel C ON A.cancelId = C.cancelId AND A.delayId = C.delayId \
 	GROUP BY A.flightDate, C.type \
 	; \
+	SELECT AVG(P.price) AS price \
+	FROM pricing P \
+	WHERE P.flightId IN (SELECT Q.flightId FROM tempview Q) \
+	; \
 	DROP VIEW IF EXISTS tempview \
 	;'
 
@@ -92,41 +97,68 @@ router.get('/:id', function(req, res) {
 	orig = req.params.id.substring(0, 3)
 	dest = req.params.id.substring(4, 7)
 	connection.query(mysql.format(sql.selectStats, [orig, dest, orig, dest]), function(err, results) {
+		
 		console.log(results[0])
 		console.log(results[2])
 		console.log(results[3])
-		console.log(results[4])
+		//console.log(JSON.stringify(_.compact(results[4])))
+		//console.log(results[5][0]['price'])
+		
 		timeDist = results[0]
 		airlineStats = results[2]
 		ontimeStats = []
 		yearStats = []
-		tmp = {'Ontime': [], 'Weather delay': [], 'Carrier delay': [], 'Late aircraft delay': [], 'National Air System delay': [], 'Canceled': [], 'Total': []}
+		tmp2 = {
+			'Ontime': Array.apply(null, new Array(12)).map(function(){return 0}),
+			'Weather delay': Array.apply(null, new Array(10)).map(function(){return 0}),
+			'Carrier delay': Array.apply(null, new Array(10)).map(function(){return 0}),
+			'Late aircraft delay': Array.apply(null, new Array(10)).map(function(){return 0}),
+			'National Air System delay': Array.apply(null, new Array(10)).map(function(){return 0}),
+			'Canceled': Array.apply(null, new Array(10)).map(function(){return 0}),
+			'Total': []
+		}
+		tmp = {}
 		results[3].forEach(function(item) {
 			ontimeStats.push([item.type, item.total])
 		})
 		results[4].forEach(function(item) {
-			tmp[item.type].push(item.total)
+			tmp[item['flightDate']] = [{}]
 		})
-		for (var i=0; i < 12; i++) {
-			tmp['Total'].push(tmp['Ontime'][i]+tmp['Weather delay'][i]+tmp['Carrier delay'][i]+tmp['Late aircraft delay'][i]+tmp['National Air System delay'][i]+tmp['Canceled'][i])
+		results[4].forEach(function(item) {
+			tmp[item['flightDate']][0][item['type']] = item['total']
+		})
+		var dates = Object.keys(tmp).sort()
+		for (var i=0; i<dates.length; i++) {
+			date = dates[i]
+			if (_.has(tmp[date][0], 'Ontime')) tmp2['Ontime'][i] = tmp[date][0]['Ontime']
+			if (_.has(tmp[date][0], 'Weather delay')) tmp2['Weather delay'][i] = tmp[date][0]['Weather delay']
+			if (_.has(tmp[date][0], 'Carrier delay')) tmp2['Carrier delay'][i] = tmp[date][0]['Carrier delay']
+			if (_.has(tmp[date][0], 'Late aircraft delay')) tmp2['Late aircraft delay'][i] = tmp[date][0]['Late aircraft delay']
+			if (_.has(tmp[date][0], 'National Air System delay')) tmp2['National Air System delay'][i] = tmp[date][0]['National Air System delay']
+			if (_.has(tmp[date][0], 'Canceled')) tmp2['Canceled'][i] = tmp[date][0]['Canceled']
 		}
 		for (var i=0; i < 12; i++) {
-			tmp['Ontime'][i] = 100*(tmp['Ontime'][i] / tmp['Total'][i])
-			tmp['Weather delay'][i] = 100*(tmp['Weather delay'][i] / tmp['Total'][i])
-			tmp['Carrier delay'][i] = 100*(tmp['Carrier delay'][i] / tmp['Total'][i])
-			tmp['Late aircraft delay'][i] = 100*(tmp['Late aircraft delay'][i] / tmp['Total'][i])
-			tmp['National Air System delay'][i] = 100*(tmp['National Air System delay'][i] / tmp['Total'][i])
-			tmp['Canceled'][i] = 100*(tmp['Canceled'][i] / tmp['Total'][i])
+			tmp2['Total'].push(tmp2['Ontime'][i]+tmp2['Weather delay'][i]+tmp2['Carrier delay'][i]+tmp2['Late aircraft delay'][i]+tmp2['National Air System delay'][i]+tmp2['Canceled'][i])
 		}
-		for (var item in tmp) {
-			if (item != 'Total') {yearStats.push({'name': item, 'data': tmp[item]})}
+		for (var i=0; i < 12; i++) {
+			tmp2['Ontime'][i] = 100*(tmp2['Ontime'][i] / tmp2['Total'][i])
+			tmp2['Weather delay'][i] = 100*(tmp2['Weather delay'][i] / tmp2['Total'][i])
+			tmp2['Carrier delay'][i] = 100*(tmp2['Carrier delay'][i] / tmp2['Total'][i])
+			tmp2['Late aircraft delay'][i] = 100*(tmp2['Late aircraft delay'][i] / tmp2['Total'][i])
+			tmp2['National Air System delay'][i] = 100*(tmp2['National Air System delay'][i] / tmp2['Total'][i])
+			tmp2['Canceled'][i] = 100*(tmp2['Canceled'][i] / tmp2['Total'][i])
 		}
+		for (var item in tmp2) {
+			if (item != 'Total') {yearStats.push({'name': item, 'data': tmp2[item]})}
+		}
+
 		res.render('route', {
 			title: req.params.id,
 			timeDist: timeDist,
 			airlineStats: airlineStats,
 			ontimeStats: ontimeStats,
-			yearStats: yearStats
+			yearStats: yearStats,
+			price: results[5][0]['price']
 		})
 	})
 })
